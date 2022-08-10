d2b49d0e1bf61920c18ad339acca96cad964e717
atoonk <andree@toonk.nl>
2022-08-10 07:53:55 -0700

    Fixing API URL

--- internal/api/api.go
+++ internal/api/api.go
@@ -78,7 +78,7 @@ func (a *MysocketAPI) Request(method string, url string, target interface{}, dat
 	jv, _ := json.Marshal(data)
 	body := bytes.NewBuffer(jv)
 
-	req, _ := http.NewRequest(method, fmt.Sprintf("%s/%s", APIURL(), url), body)
+	req, _ := http.NewRequest(method, fmt.Sprintf("%s%s", APIURL(), url), body)
 
 	//try to find the token in the environment
 	if requireAccessToken && a.AccessToken == "" {

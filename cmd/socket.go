6c0eae3b6f75a9c70612fd48c94a6559b945e907
atoonk <andree@toonk.nl>
2022-10-03 16:50:30 -0700

    No more need to check for auth emails
    we have policies now

--- cmd/socket.go
+++ cmd/socket.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -118,11 +118,6 @@ var socketCreateCmd = &cobra.Command{
 			if domain != "" {
 				allowedEmailDomains = append(allowedEmailDomains, domain)
 			}
-		}
-		if len(allowedEmailDomains) == 0 && len(allowedEmailAddresses) == 0 {
-			log.Println("No Email or Email Domains were provided.")
-			log.Println("No-one will have access.")
-			os.Exit(1)
 		}
 
 		socketType := strings.ToLower(socketType)
